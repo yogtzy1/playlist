4b89b4b9cd070d04ddba6023e4099f6aea219705
yogtzy1 <ranzyykambing@gmail.com>
2025-12-16 08:05:45 +0700

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -4,7 +4,7 @@ const playlist = [
   artist:"My Playlist",
   cover:"https://i.imgur.com/1.jpg",
   src:"https://example.com/song1.mp3",
-  lrc:"song1.lrc",
+  lrc:"song1.mp3",
   gradient:["#1DB954","#121212"]
 },
 // copy sampai 10 lagu
