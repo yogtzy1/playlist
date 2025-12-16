68b75f1366098ca3bdb3dfaeb19a9f319e8bdbb3
yogtzy1 <ranzyykambing@gmail.com>
2025-12-16 07:59:51 +0700

    Create playlist.js

---
+++ playlist.js
@@ -0,0 +1,11 @@
+const playlist = [
+{
+  title:"Song One",
+  artist:"My Playlist",
+  cover:"https://i.imgur.com/1.jpg",
+  src:"https://example.com/song1.mp3",
+  lrc:"lyrics/song1.lrc",
+  gradient:["#1DB954","#121212"]
+},
+// copy sampai 10 lagu
+];
